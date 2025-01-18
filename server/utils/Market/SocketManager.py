a1f2e91c09966a4b1bc2c4a5d1d67b07082909da
Aryan Singh <aryansingh920@outlook.com>
2025-01-18 23:35:38 +0000

    Remove Graph module and integrate WebSocketManager for real-time event broadcasting

--- server/utils/Market/SocketManager.py
+++ server/utils/Market/SocketManager.py
@@ -1,43 +1,105 @@
-"""
-Created on 18/01/2025
+# """
+# Created on 18/01/2025
 
-@author: Aryan
+# @author: Aryan
 
-Filename: SocketManager.py
+# Filename: SocketManager.py
 
-Relative Path: server/utils/Market/SocketManager.py
-"""
-
-
-"""
-SocketManager.py
-
-Creates a Socket.IO server to broadcast events in real-time.
-"""
+# Relative Path: server/utils/Market/SocketManager.py
+# """
 
 
-# Create a Socket.IO server
-import eventlet
-import socketio
-sio = socketio.Server(cors_allowed_origins="*")
-# Wrap with a WSGI app
-app = socketio.WSGIApp(sio)
+# """
+# SocketManager.py
+
+# Creates a Socket.IO server to broadcast events in real-time.
+# """
 
 
-@sio.event
-def connect(sid, environ):
-    print(f"[SocketManager] Client connected: {sid}")
+# # Create a Socket.IO server
+# import eventlet
+# import socketio
+# sio = socketio.Server(cors_allowed_origins="*")
+# # Wrap with a WSGI app
+# app = socketio.WSGIApp(sio)
 
 
-@sio.event
-def disconnect(sid):
-    print(f"[SocketManager] Client disconnected: {sid}")
+# @sio.event
+# def connect(sid, environ):
+#     print(f"[SocketManager] Client connected: {sid}")
 
 
-def broadcast_event(event_name, data):
+# @sio.event
+# def disconnect(sid):
+#     print(f"[SocketManager] Client disconnected: {sid}")
+
+
+# def broadcast_event(event_name, data):
+#     """
+#     Utility function to broadcast a given event (event_name)
+#     with data (a dict) to all connected clients.
+#     """
+#     # print(f"[SocketManager] Emitting event: {event_name}, Data: {data}")
+#     sio.emit(event_name, data)
+
+
+# WebSocketManager.py
+
+import asyncio
+import json
+import websockets
+
+# Keep track of all connected client websockets
+connected_clients = set()
+
+
+async def handle_client(websocket, path):
     """
-    Utility function to broadcast a given event (event_name)
-    with data (a dict) to all connected clients.
+    This is called whenever a new client connects.
+    We'll add the client to our set and remove when disconnected.
     """
-    # print(f"[SocketManager] Emitting event: {event_name}, Data: {data}")
-    sio.emit(event_name, data)
+    print(f"[WebSocketManager] Client connected: {websocket.remote_address}")
+    connected_clients.add(websocket)
+    try:
+        async for message in websocket:
+            # If you want to handle incoming messages from the client,
+            # you can process them here.
+            # For now, we just ignore.
+            pass
+    except websockets.ConnectionClosed:
+        pass
+    finally:
+        connected_clients.remove(websocket)
+        print(
+            f"[WebSocketManager] Client disconnected: {websocket.remote_address}")
+
+
+async def broadcast_event(event_name, data):
+    """
+    Broadcast an event to all connected clients.
+    event_name: a string naming the event (e.g. "new_order")
+    data: a Python dict with the payload to send
+    """
+    if not connected_clients:
+        return  # No clients to send to, so just return
+
+    message_dict = {
+        "event_name": event_name,
+        "data": data
+    }
+    message_str = json.dumps(message_dict)
+
+    # Gather coroutines for sending to all clients
+    await asyncio.gather(
+        *[client.send(message_str) for client in connected_clients],
+        return_exceptions=True
+    )
+
+
+def start_websocket_server(host="0.0.0.0", port=8765):
+    """
+    Starts the asyncio WebSocket server. You can call this function from your
+    main entry point (e.g. in a separate thread or directly in an asyncio loop).
+    """
+    print(f"[WebSocketManager] Starting WebSocket server on {host}:{port}")
+    return websockets.serve(handle_client, host, port)
--- server/utils/hft_simulator.py
+++ server/utils/hft_simulator.py
@@ -7,7 +7,6 @@ real-time events (orders, trades, snapshots, etc.) via Socket.IO.
 
 from Market.MarketSimulator import MarketSimulator
 from Market.dynamic_config import build_simulation_config
-from Market.Graph import plot_symbol_candlestick
 
 if __name__ == "__main__":
     # 1) Build the simulation config (dynamically):
@@ -20,14 +19,3 @@ if __name__ == "__main__":
     # 3) Run the simulation for a certain number of steps
     #    As it runs, the DataLogger inside will broadcast all events via Socket.IO
     simulator.run(steps=1000)
-
-    # 4) After the simulation finishes, we can pick a symbol CSV and visualize candlesticks
-    symbol_csv_file = "./simulation_logs/heat_1/symbol_GOOG.csv"
-    frequency = '1S'  # Frequency for resampling candlesticks
-
-    plot_symbol_candlestick(symbol_csv_path=symbol_csv_file, freq=frequency)
-
-    # If you want a Dash app to display a live-updating graph (independent from the simulator):
-    # from Market.Graph import create_dash_app
-    # dash_app = create_dash_app(symbol_csv_file, freq=frequency)
-    # dash_app.run_server(debug=True, port=8051)
